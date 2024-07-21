375c04c87d5af2022f294e062d02c4a28c2d2cdf
sujit sah <sujit8877123sah@gmail.com>
2024-07-21 17:23:31 +0545

    Advanced-Portfolio

---
+++ data/index.ts
@@ -0,0 +1,191 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Study Notion",
+      des: "StudyNotion is a fully functional ed-tech platform that enables users to create, consume, and rate educational content.",
+      img: "/studynotion.png",
+      iconLists: ["/re.svg", "/tail.svg", "/mongodb.svg", "/js.png"],
+      link: "https://mega-study-notion.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "RazorPay",
+      des: "Razorpay is a leading payment processor that provides end-to-end solutions for businesses...",
+      img: "/razorpay.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.png", "/html.webp", "/css.webp"],
+      link: "https://razorpay-website-clone-rosy.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Weather App",
+      des: "The weather application will provide users with real-time weather information, forecasts, and other weather-related data...",
+      img: "/weather.webp",
+      iconLists: ["/html.webp", "/tail.svg", "/css.webp", "/js.png"],
+      link: "https://weather-app-indol-psi-88.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "Zero Cross Game",
+      des: "a game in which two players take turns in drawing either an ` O' or an ` X' in one square of a grid consisting of nine squares...",
+      img: "/zerocross.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/js.png"],
+      link: "https://zero-cross-game.vercel.app/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non necessitatibus voluptatem reiciendis doloribus. Maiores ullam minus, quae dignissimos explicabo perspiciatis alias quasi assumenda necessitatibus id amet vero aspernatur, fuga iure, expedita labore sequi repellat saepe ratione! Libero sapiente aliquid laudantium exercitationem id repudiandae culpa nisi quaerat. Neque, quidem eveniet! Sint?",
+        name: "Sujit Sah",
+        title: "Full Stack Web Developer",
+    },
+    {
+      quote:
+        " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non necessitatibus voluptatem reiciendis doloribus. Maiores ullam minus, quae dignissimos explicabo perspiciatis alias quasi assumenda necessitatibus id amet vero aspernatur, fuga iure, expedita labore sequi repellat saepe ratione! Libero sapiente aliquid laudantium exercitationem id repudiandae culpa nisi quaerat. Neque, quidem eveniet! Sint?",
+        name: "Sujit Sah",
+        title: "Full Stack Web Developer",
+    },
+    {
+      quote:
+        " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non necessitatibus voluptatem reiciendis doloribus. Maiores ullam minus, quae dignissimos explicabo perspiciatis alias quasi assumenda necessitatibus id amet vero aspernatur, fuga iure, expedita labore sequi repellat saepe ratione! Libero sapiente aliquid laudantium exercitationem id repudiandae culpa nisi quaerat. Neque, quidem eveniet! Sint?",
+        name: "Sujit Sah",
+        title: "Full Stack Web Developer",
+    },
+    {
+      quote:
+        " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non necessitatibus voluptatem reiciendis doloribus. Maiores ullam minus, quae dignissimos explicabo perspiciatis alias quasi assumenda necessitatibus id amet vero aspernatur, fuga iure, expedita labore sequi repellat saepe ratione! Libero sapiente aliquid laudantium exercitationem id repudiandae culpa nisi quaerat. Neque, quidem eveniet! Sint?",
+        name: "Sujit Sah",
+        title: "Full Stack Web Developer",
+    },
+    {
+      quote:
+        " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non necessitatibus voluptatem reiciendis doloribus. Maiores ullam minus, quae dignissimos explicabo perspiciatis alias quasi assumenda necessitatibus id amet vero aspernatur, fuga iure, expedita labore sequi repellat saepe ratione! Libero sapiente aliquid laudantium exercitationem id repudiandae culpa nisi quaerat. Neque, quidem eveniet! Sint?",
+      name: "Sujit Sah",
+      title: "Full Stack Web Developer",
+    },
+  ];
+  
+ 
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Development",
+      desc: "Assisted in the development of a web-based platform using React.js, Tailwind CSS, Javascript.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Backend Development",
+      desc: " Assisted in the development of a web-based platform using Node.js, Express.js, and MongoDB.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Web Dev Project",
+      desc: "led the development of a mobile app for a client as a MERN Stack Developer.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      link:"https://github.com/sahsujit",
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      link:"https://x.com/sujitsah1388",
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      link:"https://www.linkedin.com/in/sujit-sah-619090274/",
+
+      img: "/link.svg",
+    },
+  ];
+  
